85c26d3b0951cec620fd36e3c0cb01a3676bdb91
Hossein Rafieizadeh <53276610+Hossein1998@users.noreply.github.com>
2025-09-04 12:51:49 +0330

    Delete dcor-graph-anomaly-detection directory

--- dcor-graph-anomaly-detection/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-DATASET=${1:-amazon}
-python train.py --dataset ${DATASET} --config configs/${DATASET}.yaml
-python eval.py  --dataset ${DATASET} --ckpt outputs/${DATASET}/best.ckpt
